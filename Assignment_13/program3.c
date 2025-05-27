7a97ab1076e3fcaaea7564cfea268d12f2d27720
ankita3040 <103482365+ankita3040@users.noreply.github.com>
2025-05-27 15:28:53 +0530

    Add files via upload

---
+++ Assignment_13/program3.c
@@ -0,0 +1,48 @@
+// Accept number of rows and number of columns from user and display below pattern.
+
+///////////////////////////////////////////////////////////////////////////////
+// File Name   :  program3.c
+// Description :  Accept number of rows and number of columns from user and display below pattern.
+// Autor       :  Ankita Anil Patil
+// Date        :  22/05/2025
+///////////////////////////////////////////////////////////////////////////////
+
+/* Input : iRow = 3   iCol = 5
+   Output : A A A A A
+            B B B B B
+            C C C C C
+*/
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char ch = '\0';
+
+    for(i = 1, ch = 'A'; i <= iRow; i++, ch++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("%c\t",ch);
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows  ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns ");
+    scanf("%d",&iValue2);
+
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
